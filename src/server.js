e7cf307f2f518279d7cb5f7194b987b831461f8b
nicohnavarro <nicohnavarro@gmail.com>
2020-03-09 00:59:01 -0300

    TSK #4: Handlebars: index

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
 
 //Initializations
@@ -8,6 +9,13 @@ const app = express();
 //Settings
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'parcials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 //Middlewares
 app.use(express.urlencoded({ extended: false }));
@@ -17,10 +25,11 @@ app.use(express.urlencoded({ extended: false }));
 
 //Routes
 app.get('/', (req, res) => {
-    res.send('hello world');
+    //res.send('hello world');
+    res.render('index');
 })
 
 //Statics Files
-app.use(express.static(path.join(__dirname, 'public ')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
