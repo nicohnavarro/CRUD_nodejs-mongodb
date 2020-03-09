1c789596d45c31b71fadbc50aed441d4b5ba2744
nicohnavarro <nicohnavarro@gmail.com>
2020-03-08 23:37:09 -0300

    TSK #2: Connecting database

---
+++ src/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+//const MONGODB_URI = process.env.MONGODB_URI;
+
+const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env;
+const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
+mongoose.connect(MONGODB_URI, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    })
+    .then(db => console.log('Database is connected'))
+    .catch(err => console.log(err));
