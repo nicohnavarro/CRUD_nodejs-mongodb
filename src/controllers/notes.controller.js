b2fc92f39a42f3b63b5e65aa6c60468ff03059d0
nicohnavarro <nicohnavarro@gmail.com>
2020-03-15 02:00:21 -0300

    TSK #13: Alert messages

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -11,6 +11,7 @@ notesCtrl.createNewNote = async(req, res) => {
     const newNote = new Note({ title, description });
     await newNote.save()
     console.log(newNote);
+    req.flash('success_msg', 'Note added Successfully');
     res.redirect('/notes');
 };
 
@@ -28,12 +29,14 @@ notesCtrl.updateNote = async(req, res) => {
     console.log(req.body);
     const { title, description } = req.body;
     await Note.findByIdAndUpdate(req.params.id, { title, description });
+    req.flash('success_msg', 'Note Updated Successfully');
     res.redirect('/notes');
 };
 
 notesCtrl.deleteNote = async(req, res) => {
     console.log(req.params.id);
     await Note.findByIdAndDelete(req.params.id);
+    req.flash('success_msg', 'Note Deleted Successfully');
     res.redirect('/notes');
 };
 
--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
 //Initializations
 const app = express();
 
@@ -22,8 +24,18 @@ app.set('view engine', '.hbs');
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
-//Global Variables
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(flash());
 
+//Global Variables
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    next();
+});
 
 //Routes
 app.use(require('./routes/index.routes'));
