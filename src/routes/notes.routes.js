cb49d2ea7c748649ba81ef129ffe0ca857897ee6
nicohnavarro <nicohnavarro@gmail.com>
2020-03-17 01:00:53 -0300

    TSK #18: Authorized Routes

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -10,18 +10,19 @@ const {
     deleteNote
 } = require('../controllers/notes.controller');
 
+const { isAuthenticated } = require('../helpers/auth');
 //New Note
-router.get('/notes/add', renderNoteForm);
-router.post('/notes/new-note', createNewNote);
+router.get('/notes/add', isAuthenticated, renderNoteForm);
+router.post('/notes/new-note', isAuthenticated, createNewNote);
 
 //Get All Notes
-router.get('/notes', renderNotes);
+router.get('/notes', isAuthenticated, renderNotes);
 
 //Edit Notes
-router.get('/notes/edit/:id', renderEditForm);
-router.put('/notes/edit/:id', updateNote);
+router.get('/notes/edit/:id', isAuthenticated, renderEditForm);
+router.put('/notes/edit/:id', isAuthenticated, updateNote);
 
 //Delete Notes
-router.delete('/notes/delete/:id', deleteNote);
+router.delete('/notes/delete/:id', isAuthenticated, deleteNote);
 
 module.exports = router;
